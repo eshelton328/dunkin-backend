b9c43f515172fc51d500615756f0bdbe4ee2efb6
eshelton328 <erik.shelton328@gmail.com>
2023-05-20 12:08:55 -0500

    [ADD] setup express/mongo backend

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import "express-async-errors";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((err, _req, res, next) => {
+  res.status(500).send("Uh oh! An unexpected error occured.")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
