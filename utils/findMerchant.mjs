aa0d73a77afa2e46a1fa10a19d5ad222b7c20b17
Erik Shelton <erik.shelton328@gmail.com>
2023-05-26 11:31:08 -0500

    [REM] removed findMerchant

--- utils/findMerchant.mjs
+++
@@ -1,8 +0,0 @@
-export const findMerchant = (array, plaidId) => {
-    for (const obj of array) {
-        if (obj.provider_ids && obj.provider_ids.plaid && obj.provider_ids.plaid.includes(plaidId)) {
-            return obj;
-        }
-    }
-    return false;
-}
